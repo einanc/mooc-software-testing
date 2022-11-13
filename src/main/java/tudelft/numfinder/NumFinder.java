f37bab7af11f21e5cb3761cccc4e4d914cf5c40f
einanc <emirinanc@gmail.com>
2022-11-13 12:21:10 +0400

    NumFinder try-catch block for handling NullPointerException

--- src/main/java/tudelft/numfinder/NumFinder.java
+++ src/main/java/tudelft/numfinder/NumFinder.java
@@ -5,14 +5,24 @@ public class NumFinder {
     private int largest = Integer.MIN_VALUE;
 
     public void find(int[] nums) {
-        for(int n : nums) {
 
-            if(n < smallest)
-                smallest = n;
-            else if (n > largest)
-                largest = n;
+        try
+        {
+            for(int n : nums) {
 
+                if(n < smallest)
+                    smallest = n;
+                if (n > largest)
+                    largest = n;
+
+
+            }
         }
+        catch(NullPointerException e)
+        {
+            System.out.print("Proivde a list of numbers \n");
+        }
+
     }
 
     public int getSmallest () {
