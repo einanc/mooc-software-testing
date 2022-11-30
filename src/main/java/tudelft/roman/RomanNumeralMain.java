c3ca99477717ebee2fe31d6d6a312935b20988fc
einanc <emirinanc@gmail.com>
2022-11-30 21:51:54 +0300

    added before each for leap year tests

---
+++ src/main/java/tudelft/roman/RomanNumeralMain.java
@@ -0,0 +1,14 @@
+package tudelft.roman;
+
+
+public class RomanNumeralMain {
+
+        public static void main (String[] args) {
+            RomanNumeral rn = new RomanNumeral();
+
+
+            System.out.println(rn.convert("XLIV"));
+            System.out.println(rn.convert("XXLLVV"));
+        }
+    }
+
